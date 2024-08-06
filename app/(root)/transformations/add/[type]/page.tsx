0daf03067a5fec9f5323ea433e211ed9aabdae43
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-08-06 19:08:18 +0300

    Add transform type

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Header from '@/components/shared/Header';
+import React from 'react';
+import { transformationTypes } from '@/constants';
+import TransformationForm from '@/components/shared/TransformationForm';
 
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+        const transformation = transformationTypes[type];
+        return (
+                <>
+                        <Header title={transformation.title} subtitle={transformation.subTitle} />
+                        <TransformationForm />
+                </>
+        );
 };
 
 export default AddTransformationTypePage;
